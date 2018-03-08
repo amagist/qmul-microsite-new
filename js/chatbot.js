2f55862424639e8552607367bd4f8080c4f05eb2
amagist <amymagistris@hotmail.co.uk>
2018-03-08 14:04:53 +0000

    styling of chatbot

--- js/chatbot.js
+++ js/chatbot.js
@@ -4,3 +4,13 @@ $(document).ready(function(){
     $(".chatbot").toggle();
   });
 });
+
+var offset = 200;
+var duration = 500;
+$(window).scroll(function() {
+  if ($(this).scrollTop() > offset) {
+    $('#chatbot-button').fadeIn(400);
+  } else {
+    $('#chatbot-button').fadeOut(400);
+  }
+});
